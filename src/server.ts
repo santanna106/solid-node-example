3824265c55a594e36b51dcccbb97160e6e58d89a
Gabriel Andrade <gabriel.santana@gmail.com>
2022-09-01 05:29:49 -0300

    last class

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import express from 'express';
+import { categoriesRoutes } from './routes/cateogries.routes';
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/categories",categoriesRoutes);
 
 app.get('/', (request, response) => response.json({ message: 'Hello World' }));
 
