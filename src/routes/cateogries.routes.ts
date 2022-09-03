a62a359ec9041dc6c0821677ef09d549dc753539
Gabriel Andrade <gabriel.santana@gmail.com>
2022-09-03 05:11:50 -0300

    reading rows with csv-parse

--- src/routes/cateogries.routes.ts
+++ src/routes/cateogries.routes.ts
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
+
 import { CategoriesRepository } from '../modules/cars/repositories/implementations/CategoriesRepository';
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 import { listCategoryController } from '../modules/cars/useCases/listCategories';
+import { importCategoryController } from '../modules/cars/useCases/importCategory'
 
 const categoriesRoutes = Router();
+
+const upload = multer({
+    dest: "./tmp",
+});
 
 categoriesRoutes.post("/", (request,response) => {
     return createCategoryController.handle(request,response);
@@ -12,6 +19,11 @@ categoriesRoutes.post("/", (request,response) => {
 
 categoriesRoutes.get("/", (request,response) => {
     return listCategoryController.handle(request,response);
+});
+
+
+categoriesRoutes.post("/import", upload.single("file"),(request,response) => {
+    return importCategoryController.handle(request,response);
 })
 
 export { categoriesRoutes }
