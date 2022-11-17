309dcc9b6ff2e06c207ccc734c6e21c626de2690
Gabriel Andrade <gabriel.santana@gmail.com>
2022-11-17 03:44:52 -0300

    Criação da rotina de upload

--- src/middlewares/ensureAuthenticate.ts
+++ src/middlewares/ensureAuthenticate.ts
@@ -25,6 +25,10 @@ export async function ensureAuthenticated(request:Request,response:Response,next
             throw new AppError("User does not exists",401);
         }
 
+        request.user = {
+           id:user_id
+        }
+
         next();
     } catch {
         throw new AppError("Invalid token !",401)
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -2,18 +2,21 @@ import { Router } from 'express';
 import multer from 'multer';
 
 import { CreateUserController } from '../modules/accounts/useCases/createUser/CreateUserController';
+import { UpdateUserAvatarController } from '../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController';
 import { ListCategoryController } from '../modules/cars/useCases/listCategories/ListCategoryController';
 
 
 const usersRoutes = Router();
 
 const upload = multer({
-    dest: "./tmp",
+    dest: "avatar",
 });
 
 const createUserController = new CreateUserController();
 
 const listCategoryController = new ListCategoryController();
+
+const updateUserController = new UpdateUserAvatarController();
 
 usersRoutes.post("/", createUserController.handle);
 
@@ -21,6 +24,11 @@ usersRoutes.post("/", createUserController.handle);
 usersRoutes.get("/", (request,response) => {
     return listCategoryController.handle(request,response);
 });
+
+usersRoutes.patch(
+    "/avatar",
+    upload.single("file"),
+    updateUserController.handle);
 
 
 
